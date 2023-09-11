50f8c326f47ef100bcbec1e3059d3540db1de8b7
mariamwechtetikhrayfi <mariamwechtetikhrayfi@gmail.com>
2023-09-11 16:22:16 +0300

    first commit

---
+++ src/main/java/com/example/helloworldpipeline/HelloWorldPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloworldpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/helloworldpipeline/HelloWorldPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworldpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
